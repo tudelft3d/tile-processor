ff88bc3734ae90ac3668b5a825da862dee99278f
Balázs Dukai <balazs.dukai@gmail.com>
2021-09-10 17:17:53 +0200

    Bump version: 0.3.6 → 0.3.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Balázs Dukai'
 author = 'Balázs Dukai'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.6'
+release = '0.3.7'
 
 
 # -- General configuration ---------------------------------------------------
